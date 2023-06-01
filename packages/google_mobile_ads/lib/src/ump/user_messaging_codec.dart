a79f354eadfcb9d0fca4bb9c8b3e47f6b7d3c2e0
MCYBA <36483874+MCYBA@users.noreply.github.com>
2023-06-01 16:22:40 +0300

    Add import 'package:flutter/foundation.dart';

--- packages/google_mobile_ads/lib/src/ump/user_messaging_codec.dart
+++ packages/google_mobile_ads/lib/src/ump/user_messaging_codec.dart
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 import 'consent_form_impl.dart';
